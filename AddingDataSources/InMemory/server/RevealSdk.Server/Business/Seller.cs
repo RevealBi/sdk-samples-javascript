6d8493b4adeba60006adfd62f81ea55555801c9f
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-02-01 12:08:44 -0700

    added in-memory sample

---
+++ AddingDataSources/InMemory/server/RevealSdk.Server/Business/Seller.cs
@@ -0,0 +1,8 @@
+﻿namespace RevealSdk.Server.Business
+{
+    public class Seller
+    {
+        public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
