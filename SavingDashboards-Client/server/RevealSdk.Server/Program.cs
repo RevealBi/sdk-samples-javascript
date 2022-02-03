d209480d9c7e1a904d8a5458c5f5d3d69464da28
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-02-02 17:36:11 -0700

    added client saving sample

---
+++ SavingDashboards-Client/server/RevealSdk.Server/Program.cs
@@ -0,0 +1,45 @@
+using Reveal.Sdk;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddReveal();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowAll",
+    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+  );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseCors("AllowAll");
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Map("/isduplicatename/{name}", (string name) =>
+{
+    var filePath = Path.Combine(Environment.CurrentDirectory, "Dashboards");
+    return File.Exists($"{filePath}/{name}.rdash");
+});
+
+app.Run();
