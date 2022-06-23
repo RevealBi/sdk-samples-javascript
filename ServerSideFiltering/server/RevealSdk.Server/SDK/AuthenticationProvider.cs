d876d2c5215afb74b2c0be8e7d1516b0f1cc8959
INFRAGISTICS\PVillarmarzo <PVillarmarzo@infragistics.com>
2022-06-23 13:56:52 -0300

    I have done some corrections to readme file and fixed the warning messages

--- ServerSideFiltering/server/RevealSdk.Server/SDK/AuthenticationProvider.cs
+++ ServerSideFiltering/server/RevealSdk.Server/SDK/AuthenticationProvider.cs
@@ -5,8 +5,8 @@ namespace RevealSdk.Server.SDK
     public class AuthenticationProvider : IRVAuthenticationProvider
     {
         public Task<IRVDataSourceCredential> ResolveCredentialsAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
-        {
-            IRVDataSourceCredential userCredential = null;
+        { 
+            IRVDataSourceCredential userCredential = new RVUsernamePasswordDataSourceCredential();
             if (dataSource is RVSqlServerDataSource) userCredential = new RVUsernamePasswordDataSourceCredential("Reveal", "Reveal");
             return Task.FromResult(userCredential);
         }
--- ServerSideFiltering/server/RevealSdk.Server/SDK/ServerSideFiltering.cs
+++ ServerSideFiltering/server/RevealSdk.Server/SDK/ServerSideFiltering.cs
@@ -4,31 +4,29 @@ namespace RevealSdk.Server.SDK
 {
     public class ServerSideFiltering : IRVObjectFilter
     {
-        public Task<bool> Filter(IRVUserContext userContext, RVDashboardDataSource dataSource) // this is a filter that goes through all base of date on the server
+        public Task<bool> Filter(IRVUserContext userContext, RVDashboardDataSource dataSource) // this is a filter that goes through all databases on the server
         {
-            var allowedList = new List<String>() { "NorthwindTraders" }; //here we indicate a list of databases with which we want to work
+            var allowedList = new List<string>() { "Northwind" }; //here we indicate a list of databases with which we want to work
 
             if (dataSource != null)
             {
-                if (dataSource is RVSqlServerDataSource) // we consult if it is a SQL DB
+                if (dataSource is RVSqlServerDataSource dataSQL) // we consult if it is a SQL DB and cast the generic data source to SQL to be able to access its attributes
                 {
-                    RVSqlServerDataSource SqlDataSource = (RVSqlServerDataSource)dataSource;  // we cast the generic data source to SQL to be able to access its attributes
-                    if (allowedList.Contains(SqlDataSource.Database)) return Task.FromResult(true); //if the name of database is includes in de allowed list return true
+                    if (allowedList.Contains(dataSQL.Database)) return Task.FromResult(true);
                 }
             }
             return Task.FromResult(false); 
         }
 
-        public Task<bool> Filter(IRVUserContext userContext, RVDataSourceItem dataSourceItem) // this is a filter that goes through all element on DB allowed
+        public Task<bool> Filter(IRVUserContext userContext, RVDataSourceItem dataSourceItem) 
         {
-            var excludedsList = new List<String>() { "Customer", "Supplier" }; // here we indicate a list of tables which we want to block
+            var excludedsList = new List<string>() { "Customers", "Suppliers" }; // here we indicate a list of tables which we want to block
 
             if (dataSourceItem != null)
             {
-                if (dataSourceItem is RVSqlServerDataSourceItem) // we consult if it is a SQL DB item
+                if (dataSourceItem is RVSqlServerDataSourceItem dataSQLItem) // we consult if it is a SQL DB item and cast the generic data source item to SQL item to be able to access its attributes
                 {
-                    RVSqlServerDataSourceItem SqlDataSourceItem = (RVSqlServerDataSourceItem)dataSourceItem;  // we cast the generic data source item to SQL item to be able to access its attributes
-                    if (excludedsList.Contains(SqlDataSourceItem.Table)) return Task.FromResult(false); //if the name of table is excludeds list return false
+                    if (excludedsList.Contains(dataSQLItem.Table)) return Task.FromResult(false); 
                 }
             }
             return Task.FromResult(true);
