0f72401f1e117e146d062514eacb7bbdd253938d
Brian Lagunas <brianlagunas@users.noreply.github.com>
2023-08-24 15:26:36 -0600

    removed old data sources samples

--- AddingDataSources/InMemory/server/aspnet/RevealSdk.Server/Business/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RevealSdk.Server.Business
-{
-    public class Product
-    {
-        public string Name { get; set; } = string.Empty;
-        public double UnitPrice { get; set; }
-    }
-}
