ae756512565051b42a223f44a11a733f647c10b7
Maicol González <mgonzalez@infragistics.com>
2023-11-20 11:02:19 -0300

    added asp.net sample for MongoDB

---
+++ DataSources/MongoDB/server/aspnet/RevealSdk.Server/Reveal/AuthenticationProvider.cs
@@ -0,0 +1,22 @@
+﻿using Reveal.Sdk;
+using Reveal.Sdk.Data;
+using Reveal.Sdk.Data.MongoDB;
+
+namespace RevealSdk.Server.Reveal
+{
+    public class AuthenticationProvider : IRVAuthenticationProvider
+    {
+        public Task<IRVDataSourceCredential> ResolveCredentialsAsync(IRVUserContext userContext,
+            RVDashboardDataSource dataSource)
+        {
+            IRVDataSourceCredential userCredential = new RVUsernamePasswordDataSourceCredential();
+            if (dataSource is RVMongoDBDataSource)
+            {
+                userCredential =
+                    new RVUsernamePasswordDataSourceCredential("username", "password", "authenticationDatabase");
+            }
+
+            return Task.FromResult(userCredential);
+        }
+    }
+}
