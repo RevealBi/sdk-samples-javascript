8a3b576365bda7667394b3aa2753b0869075d75d
Maicol González <mgonzalez@infragistics.com>
2023-11-22 18:35:42 -0300

    added Oracle samples

---
+++ DataSources/Oracle/server/nodejs-ts/src/app.ts
@@ -0,0 +1,67 @@
+import express, {Application} from 'express';
+import reveal,
+{
+    IRVUserContext,
+    RevealOptions,
+    RVDashboardDataSource,
+    RVDataSourceItem,
+    RVOracleDataSource,
+    RVOracleDataSourceItem,
+    RVOracleServiceDataSource,
+    RVOracleSIDDataSource,
+    RVUsernamePasswordDataSourceCredential
+} from 'reveal-sdk-node';
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+
+const authenticationProvider = async (userContext: IRVUserContext | null, dataSource: RVDashboardDataSource) => {
+    if (dataSource instanceof RVOracleDataSource) {
+        return new RVUsernamePasswordDataSourceCredential("username", "password");
+    }
+    return null;
+}
+
+const dataSourceItemProvider = async (userContext: IRVUserContext | null, dataSourceItem: RVDataSourceItem) => {
+    if (dataSourceItem instanceof RVOracleDataSourceItem) {
+
+        //update underlying data source
+        dataSourceProvider(userContext, dataSourceItem.dataSource);
+
+        //only change the table if we have selected our data source item
+        if (dataSourceItem.id === "MyOracleDataSourceItem") {
+            dataSourceItem.table = "your-table";
+        }
+    }
+    return dataSourceItem;
+}
+
+const dataSourceProvider = async (userContext: IRVUserContext | null, dataSource: RVDashboardDataSource) => {
+    //using SID
+    if (dataSource instanceof RVOracleSIDDataSource) {
+        dataSource.host = "your-host";
+        dataSource.database = "your-database";
+        dataSource.sID = "your-sid";
+    }
+
+    //using service name
+    if (dataSource instanceof RVOracleServiceDataSource) {
+        dataSource.host = "your-host";
+        dataSource.database = "your-database";
+        dataSource.service = "your-service-name";
+    }
+    return dataSource;
+}
+
+const revealOptions: RevealOptions = {
+    authenticationProvider: authenticationProvider,
+    dataSourceProvider: dataSourceProvider,
+    dataSourceItemProvider: dataSourceItemProvider,
+}
+app.use('/', reveal(revealOptions));
+
+app.listen(5111, () => {
+    console.log(`Reveal server accepting http requests`);
+});
---
+++ DataSources/Oracle/server/spring-boot-jersey/src/main/java/com/server/reveal/DataSourceProvider.java
@@ -0,0 +1,42 @@
+package com.server.reveal;
+
+import com.infragistics.reveal.sdk.api.IRVDataSourceProvider;
+import com.infragistics.reveal.sdk.api.IRVUserContext;
+import com.infragistics.reveal.sdk.api.model.*;
+
+import java.util.Objects;
+
+public class DataSourceProvider implements IRVDataSourceProvider {
+    public RVDataSourceItem changeDataSourceItem(IRVUserContext userContext, String dashboardsID, RVDataSourceItem dataSourceItem) {
+
+        if (dataSourceItem instanceof RVOracleDataSourceItem oracleDataSourceItem) {
+
+            //update underlying data source
+            changeDataSource(userContext, dataSourceItem.getDataSource());
+
+            //only change the table if we have selected our custom data source item
+            if (Objects.equals(dataSourceItem.getId(), "MyOracleDataSourceItem")) {
+                oracleDataSourceItem.setTable("ORDERS");
+            }
+        }
+        return dataSourceItem;
+    }
+
+    public RVDashboardDataSource changeDataSource(IRVUserContext userContext, RVDashboardDataSource dataSource) {
+        //using SID
+        if (dataSource instanceof RVOracleSIDDataSource oracleSIDDataSource) {
+            oracleSIDDataSource.setHost("your-host");
+            oracleSIDDataSource.setDatabase("your-database");
+            oracleSIDDataSource.setSID("your-sid");
+        }
+
+        //using service name
+        if (dataSource instanceof RVOracleServiceDataSource oracleServiceDataSource) {
+            oracleServiceDataSource.setHost("your-host");
+            oracleServiceDataSource.setDatabase("your-database");
+            oracleServiceDataSource.setService("your-service-name");
+        }
+
+        return dataSource;
+    }
+}
