da20c8bb8a76a417f2d94f68dce063e771e7209a
Maicol González <mgonzalez@infragistics.com>
2023-12-11 11:01:58 -0300

    added CustomizingMapTiles sample

---
+++ CustomizingMapTiles/server/RevealSdk.Server/Program.cs
@@ -0,0 +1,43 @@
+using Reveal.Sdk;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddReveal(revealSetupBuilder =>
+{
+    revealSetupBuilder.AddSettings(settings => { settings.LocalFileStoragePath = "Data"; });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("AllowAll");
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
