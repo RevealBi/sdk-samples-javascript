75baf18a9f9ff45d270a8deb030cf2846afcd4b2
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2024-09-10 16:59:38 -0600

    added ms analysis services sample

---
+++ DataSources/MicrosoftAnalysisServices/server/aspnet/RevealSdk.Server/Reveal/AuthenticationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Reveal.Sdk;
+using Reveal.Sdk.Data;
+using Reveal.Sdk.Data.Microsoft.AnalysisServices;
+
+namespace RevealSdk.Server.Reveal
+{
+    public class AuthenticationProvider : IRVAuthenticationProvider
+    {
+        public Task<IRVDataSourceCredential> ResolveCredentialsAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
+        {
+            IRVDataSourceCredential userCredential = new RVUsernamePasswordDataSourceCredential();
+            if (dataSource is RVAnalysisServicesDataSource)
+            {
+                userCredential = new RVUsernamePasswordDataSourceCredential("username", "password", "domain");
+            }
+            return Task.FromResult(userCredential);
+        }
+    }
+}
---
+++ DataSources/MicrosoftAnalysisServices/server/aspnet/RevealSdk.Server/Reveal/DataSourceProvider.cs
@@ -0,0 +1,40 @@
+﻿using Reveal.Sdk;
+using Reveal.Sdk.Data;
+using Reveal.Sdk.Data.Microsoft.AnalysisServices;
+
+namespace RevealSdk.Server.Reveal
+{
+    public class DataSourceProvider: IRVDataSourceProvider
+    {
+        public Task<RVDataSourceItem> ChangeDataSourceItemAsync(IRVUserContext userContext, string dashboardId, RVDataSourceItem dataSourceItem)
+        {
+            ChangeDataSourceAsync(userContext, dataSourceItem.DataSource);
+
+            if (dataSourceItem is RVAnalysisServicesDataSourceItem dsi)
+            {
+                if (dsi.Id == "AnalysisServicesDataSourceItem")
+                {
+                    dsi.Cube = "cube";
+                }
+            }
+
+            return Task.FromResult(dataSourceItem);
+        }
+
+        public Task<RVDashboardDataSource> ChangeDataSourceAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
+        {
+            if (dataSource is RVHttpAnalysisServicesDataSource ds)
+            {
+                ds.Url = "url";
+                ds.Catalog = "catalog";
+            }
+            else if (dataSource is RVNativeAnalysisServicesDataSource nds)
+            {
+                nds.Host = "host";
+                nds.Catalog = "catalog";
+            }
+
+            return Task.FromResult(dataSource);
+        }
+    }
+}
