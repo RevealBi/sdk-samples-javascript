ac4bb63ff9e360155e31beab3bf69e3e9056cb99
Jason Beres <jasonberes@gmail.com>
2025-08-07 08:55:57 -0400

    Update main.js

--- DataSources/Databricks/server/nodejs-js/main.js
+++ DataSources/Databricks/server/nodejs-js/main.js
@@ -6,6 +6,20 @@ const app = express();
 
 app.use(cors()); // DEVELOPMENT only! In production, configure appropriately.
 
+
+// Step 1: User context provider
+const userContextProvider = (request) => {
+  console.log('All request headers:', request.headers); 
+  const userId = request.headers['x-header-customerid']; 
+  if (!userId) {
+    console.warn('x-header-customerId is not found in headers');
+  }
+  var props = new Map();
+  props.set("userId", userId); 
+  return new reveal.RVUserContext(userId, props);
+};
+
+// Step 2: Authentication
 const authenticationProvider = async (userContext, dataSource) => {
 	if (dataSource instanceof reveal.RVDatabricksDataSource) {
 		return new reveal.RVBearerTokenDataSourceCredential("your_personal_access_token", "your_userid");
@@ -13,6 +27,19 @@ const authenticationProvider = async (userContext, dataSource) => {
 	return null;
 }
 
+// Step 3: Connect to database
+const dataSourceProvider = async (userContext, dataSource) => {
+	if (dataSource instanceof reveal.RVDatabricksDataSource) {
+		dataSource.host = "your_server_host";
+        dataSource.port = 443; // 443 is the default port
+        dataSource.httpPath = "your_server_http_path";
+        dataSource.database = "your_database_name";
+        dataSource.schema = "your_schema_name";
+	}
+	return dataSource;
+}
+
+// Step 4: Create custom data items
 const dataSourceItemProvider = async (userContext, dataSourceItem) => {
 	if (dataSourceItem instanceof reveal.RVDatabricksDataSourceItem) {
 
@@ -27,21 +54,35 @@ const dataSourceItemProvider = async (userContext, dataSourceItem) => {
 	return dataSourceItem;
 }
 
-const dataSourceProvider = async (userContext, dataSource) => {
-	if (dataSource instanceof reveal.RVDatabricksDataSource) {
-		dataSource.host = "your_server_host";
-        dataSource.port = 443; // 443 is the default port
-        dataSource.httpPath = "your_server_http_path";
-        dataSource.database = "your_database_name";
-        dataSource.schema = "your_schema_name";
-	}
-	return dataSource;
+// Step 5: Load Dashboard (Optional)
+const dashboardProvider = async (userContext, dashboardId) => {
+  console.log(`Loading dashboard ${dashboardId}`);
+  return fs.createReadStream(`${dashboardDefaultDirectory}/${dashboardId}.rdash`);
 }
 
+// Step 6: Write Dashboard (Optional)
+const dashboardStorageProvider = async (userContext, dashboardId, stream) => {
+  console.log(`Saving dashboard ${dashboardId}`);
+  const userId = userContext.properties.get("userId");
+  let savePath;
+
+  if (userId === 'ALFKI') {
+    savePath = `${dashboardDefaultDirectory}/${dashboardId}.rdash`;
+    console.log(`Saving dashboard ${dashboardId} for user ${userId} to ${savePath}`);
+  } else {
+    savePath = `${dashboardDefaultDirectory}/${dashboardId}.rdash`;
+  }
+
+  await pipelineAsync(stream, fs.createWriteStream(savePath));
+};
+
 const revealOptions = {
-    authenticationProvider: authenticationProvider,
-    dataSourceProvider: dataSourceProvider,
-	dataSourceItemProvider: dataSourceItemProvider,
+userContextProvider: userContextProvider,
+authenticationProvider: authenticationProvider,
+dataSourceProvider: dataSourceProvider,
+dataSourceItemProvider: dataSourceItemProvider,
+//dashboardProvider: dashboardProvider,
+//dashboardStorageProvider: dashboardStorageProvider,
 }
 app.use('/', reveal(revealOptions));
 
